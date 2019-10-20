280c0fbf59106689f60e551eedcbbcb69143ff2d
SupriyaAdhanki <47866043+SupriyaAdhanki@users.noreply.github.com>
2019-10-19 23:34:14 -0500

    Update maxProfit.java

--- Java/src/maxProfit.java
+++ Java/src/maxProfit.java
@@ -7,15 +7,38 @@ public class maxProfit {
   }
 
   public int maxProfit(int[] prices) {
-    int temp = 0, max = 0;
+    int temp = 0, diff = 0,min=Integer.MAX_VALUE;
     for (int i = 0; i < prices.length; i++) {
-      temp = prices[i];
-      for (int j = i + 1; j < prices.length; j++) {
-        if (prices[j] - temp > max)
-          max = prices[j] - temp;
+      if(prices[i]<min) {
+        min=prices[i];
+      }else {
+        if(prices[i]-min > diff) {
+          diff = prices[i]-min;
+        }
       }
     }
-    return max;
+    return diff;
+  }
+  
+  
+  //multiple transations
+  
+  public int maxProfits(int[] prices) {
+    int temp = 0, diff = 0,min=Integer.MAX_VALUE,maxStock=0;
+    for (int i = 0; i < prices.length; i++) {
+      if(prices[i]<min) {
+        min=prices[i]; //buy
+      }else {
+        if(prices[i]-min > diff) {
+          diff = prices[i]-min;
+        }else {
+          maxStock=maxStock+diff;
+          diff=0;
+          min=Integer.MAX_VALUE;
+        }
+      }
+    }
+    return diff;
   }
 
 }
